a19292cf7f065509d1e58cf74335b38b02057408
Lucilvio Lima <lucilvio@timipro.com.br>
2019-10-18 03:11:05 +0100

    Ajust on decimal parse

--- src/lucilvio.solo.Webills/Lucilvio.Solo.Webills.Web/Home/EditIncomeCommandAdapter.cs
+++ src/lucilvio.solo.Webills/Lucilvio.Solo.Webills.Web/Home/EditIncomeCommandAdapter.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Globalization;
 using Lucilvio.Solo.Webills.Domain.User;
-using Lucilvio.Solo.Webills.UseCases.Contracts.EditIncome;
 using Lucilvio.Solo.Webills.Web.Home.EditIncome;
+using Lucilvio.Solo.Webills.UseCases.Contracts.EditIncome;
 
 namespace Lucilvio.Solo.Webills.Web.Home
 {
@@ -16,14 +16,15 @@ namespace Lucilvio.Solo.Webills.Web.Home
             base.Name = viewModel.Name;
             base.Number = new Guid(viewModel.Number);
             base.Date = DateTime.ParseExact(viewModel.Date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
-            base.Value = new TransactionValue(decimal.Parse(viewModel.Value, 
-                NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands, 
-                new NumberFormatInfo
-            {
-                PerMilleSymbol = ".",
-                CurrencyDecimalSeparator = ",",
-                NumberDecimalSeparator = ","
-            }));
+
+            var nf = new CultureInfo("en-US", false).NumberFormat;
+            nf.NumberDecimalSeparator = ",";
+            nf.PerMilleSymbol = ".";
+            nf.CurrencyDecimalSeparator = " ";
+            nf.CurrencySymbol = "€";
+            nf.NumberGroupSeparator = ".";
+
+            base.Value = new TransactionValue(decimal.Parse(viewModel.Value, nf));
         }
     }
 }
