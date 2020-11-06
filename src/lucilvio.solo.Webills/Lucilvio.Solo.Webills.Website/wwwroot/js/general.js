e945f4529629a24ec52b6d3f18134d6460af84a8
Lucilvio Lima <Lucilvio.Lima@saphety.com>
2020-11-06 20:04:32 +0000

    added error and sucess messages

---
+++ src/lucilvio.solo.Webills/Lucilvio.Solo.Webills.Website/wwwroot/js/general.js
@@ -0,0 +1,17 @@
+﻿(function ($, blockModule) {
+    "strict";
+
+    $(function () {
+        blockModule.unblockScreen();
+
+        $("a").on("click", function () {
+            blockModule.blockScreen();
+        });
+    });
+
+    $("form").on("submit", function () {
+        blockModule.blockScreen();
+    });
+
+    $(document).ajaxStart(blockModule.blockScreen).ajaxStop(blockModule.unblockScreen);
+})(jQuery, blockModule);
---
+++ src/lucilvio.solo.Webills/Lucilvio.Solo.Webills.Website/wwwroot/js/modules/message-modules.js
@@ -0,0 +1,45 @@
+﻿var messageModule = function ($) {
+    "strict";
+
+    function showErrorMessage(msg) {
+        toastr.options = {
+            "closeButton": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "showDuration": "150000",
+            "hideDuration": "1000",
+            "timeOut": "22000",
+            "extendedTimeOut": "5000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+
+        toastr.error(msg, "Sorry, something goes wrong....<br />")
+    }
+
+    function showSuccessMessage(msg) {
+        toastr.options = {
+            "closeButton": true,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "4000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+
+        toastr.success(msg, "Yey!<br />");
+    }
+
+    return {
+        showErrorMessage: showErrorMessage,
+        showSuccessMessage: showSuccessMessage
+    }
+
+}(jQuery);
